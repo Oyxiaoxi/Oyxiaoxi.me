fe01a482a6d9b6290859496c90137815b587f712
Oyxiaoxi <Oyxiaoxi@hotmail.com>
2022-06-01 00:10:43 +0800

    feat: Modify vite.config & App

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
-
+useHead({
+  meta: [
+    { property: 'og:title', content: 'Oyxiaoxi' },
+    { property: 'og:image', content: 'https://oyxiaoxi.me/avatar.png' },
+    { name: 'description', content: 'Oyxiaoxi\'s Portfolio' },
+    { name: 'twitter:card', content: 'summary' },
+    { name: 'twitter:creator', content: '@Oyxiaoxi' },
+  ],
+})
 </script>
 
 <template>
