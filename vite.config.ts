f3f2d35a0ec1c8e4cd1ac9aee0d4b106a52182b9
Oyxiaoxi <Oyxiaoxi@hotmail.com>
2022-05-31 21:50:24 +0800

    feat: Modify vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,55 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import Vue from '@vitejs/plugin-vue'
+import type { UserConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Pages from 'vite-plugin-pages'
+import fs from 'fs-extra'
+import matter from 'gray-matter'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+const config: UserConfig = {
+  resolve: {
+    alias: [
+      { find: '~', replacement: `${resolve(__dirname, 'src')}/` },
+    ],
+  },
+  optimizeDeps: {
+    include: [
+      'vue',
+      'vue-router',
+      '@vueuse/core',
+    ],
+  },
+  plugins: [
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        '@vueuse/core',
+        '@vueuse/head',
+      ],
+    }),
+
+    Vue({
+      include: [/\.vue$/, /\.md$/],
+      reactivityTransform: true,
+    }),
+
+    Pages({
+      extensions: ['vue', 'md'],
+      pagesDir: 'pages',
+      extendRoute(route) {
+        const path = resolve(__dirname, route.component.slice(1))
+        if (!path.includes('projects.md')) {
+          const md = fs.readFileSync(path, 'utf-8')
+          const { data } = matter(md)
+          route.meta = Object.assign(route.meta || {}, { frontmatter: data })
+        }
+
+        return route
+      },
+    }),
+
+  ],
+}
+
+export default config
