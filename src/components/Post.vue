a1c95135eeedc4b36fe723fed7285c08d8b212ce
Oyxiaoxi <Oyxiaoxi@hotmail.com>
2023-01-04 12:06:58 +0800

    chore: Update Post Components

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -12,8 +12,8 @@ const router = useRouter()
 const route = useRoute()
 const content = ref<HTMLDivElement>()
 
-const base = 'https://antfu.me'
-const tweetUrl = computed(() => `https://twitter.com/intent/tweet?text=${encodeURIComponent(`Reading @antfu7\'s ${base}${route.path}\n\nI think...`)}`)
+const base = 'https://oyxiaoxi.me'
+const tweetUrl = computed(() => `https://twitter.com/intent/tweet?text=${encodeURIComponent(`Reading @Oyxiaoxi\'s ${base}${route.path}\n\nI think...`)}`)
 
 onMounted(() => {
   const navigate = () => {
