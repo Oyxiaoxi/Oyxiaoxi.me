2bae4dadfa7da05331fa809080de615039e7c4f7
欧阳晓锡 <oyxiaoxi@Oyxiaoxi.local>
2023-05-08 23:39:05 +0800

    fix: improve mobile layout & style

--- src/components/ListPosts.vue
+++ src/components/ListPosts.vue
@@ -76,28 +76,28 @@ function getGroupName(p: Post) {
           class="item block font-normal mb-6 mt-2 no-underline"
         >
           <li class="no-underline" flex="~ col md:row gap-2 md:items-center">
-            <div class="title text-lg leading-1.2em" flex gap-2>
+            <div class="title text-lg leading-1.2em" flex="~ gap-2 wrap">
               <span
                 v-if="route.lang === 'zh'"
-                align-middle
-                class="text-xs bg-zinc:15 text-zinc5 rounded px-1 py-0.5 md:ml--12 mr2 my-auto"
+                align-middle flex-none
+                class="text-xs bg-zinc:15 text-zinc5 rounded px-1 py-0.5 md:ml--12 mr2 my-auto hidden md:block"
               >中文</span>
               <span align-middle>{{ route.title }}</span>
               <span
                 v-if="route.inperson"
-                align-middle op50
+                align-middle op50 flex-none
                 i-ri:group-2-line
                 title="In person"
               />
               <span
                 v-if="route.recording || route.video"
-                align-middle op50
+                align-middle op50 flex-none
                 i-ri:film-line
                 title="Provided in video"
               />
               <span
                 v-if="route.radio"
-                align-middle op50
+                align-middle op50 flex-none
                 i-ri:radio-line
                 title="Provided in radio"
               />
